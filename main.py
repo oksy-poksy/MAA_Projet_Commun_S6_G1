e795476c7dbf910c8927655c2fd717769f447cb5
p.l <pierre.lry31@gmail.com>
2026-01-22 10:13:29 +0100

    structure du programme

---
+++ main.py
@@ -0,0 +1,23 @@
+class Traitement :
+    def decoupe_en_pixel(self): #avec numpy
+        pass
+    def binarisation(self): #mettre la valeur des pixels
+        pass
+    def histogramme(self): #entree : image binarisée, sortie : "list numpy"
+        pass
+    def selection_colonnes(self): #on sait pas encore comment on va faire
+        pass
+    def correction2pente(self): #inutile cest carré dans l'axe
+        pass
+    def correction_inclinaison(self): #inutile cest carré dans l'axe
+        pass
+class Reseau2Neurone :
+    def forward(self):
+        pass
+    def backward(self):
+        pass
+
+class Entrainement :
+    pass
+
+
